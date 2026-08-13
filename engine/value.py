fab3c38c1d546e37502ccadb02a47908a30c9bb6
Siddharth Mishra <sidd.dev006@gmail.com>
2026-08-13 12:23:35 +0530

    Completed milestone 2

--- engine/value.py
+++ engine/value.py
@@ -1,12 +1,31 @@
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, parents = None):
         self.data = data
+        self.parents = parents
+    def __repr__(self):
+        return str(self.data)
     def __add__(self, other):
         out = self.data + other.data
+        out = Value(out, (self, other))
         return out
+    def __mul__(self, other):
+        out = self.data * other.data
+        out = Value(out, (self, other))
+        return out
+    #def __radd__(self, other):
+
         
 v = Value(5)
 a = Value(5)
 b = Value(10)
+c = Value(15, (a,b))
+#print(c.parents)
+d = a*b
+print(d)
+print(type(d.parents[0]))
+'''
+print(a+b)
 print(type(a+b))
 print(v.data)
+print(a.data) #print always returns a str
+'''
