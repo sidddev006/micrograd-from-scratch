c7bfb697cdb7a656f64583b144c6b22bee101661
Siddharth Mishra <sidd.dev006@gmail.com>
2026-08-13 17:28:25 +0530

    Milestone 4 extension: tanh operation with verified analytical + numerical gradient

--- engine/value.py
+++ engine/value.py
@@ -1,3 +1,4 @@
+import math
 class Value:
     def __init__(self, data, parents = None):
         self.data = data
@@ -24,6 +25,14 @@ class Value:
             other.grad += self.data * out.grad
         out._backward = _backward
         return out
+
+    def tanh(self):
+        out = (math.exp(2*self.data) - 1)/(math.exp(2*self.data) + 1)
+        out = Value(out, (self, ))
+        def _backward():
+            self.grad += (1 - out.data **2) * out.grad
+        out._backward = _backward
+        return out
     
     def backward(self):
         order = []
@@ -44,7 +53,7 @@ class Value:
                 node._backward()
     #def __radd__(self, other):
 
-       
+''''       
 v = Value(5)
 a = Value(5)
 b = Value(10)
@@ -52,28 +61,28 @@ c = Value(15, (a,b))
 #print(c.parents)
 d = a*b
 d.grad = 1.0
-''''
+
 d._backward()
 print(a.grad, b.grad)#prints 10.0 5.0
 e = a+b
 e.grad = 1.0
 e._backward()
 print(a.grad, b.grad) #prints 11.0 6.0
-'''
+
 x = Value(3)
 y = Value(4)
 z = x+y
 w = z*x
-'''
+
+
 w.grad = 1.0
 w._backward()
 z._backward()
 print(x.grad, z.grad, y.grad)# prints 10.0 3.0 3.0
-'''
 w2 = z * x
 w2.backward()
 print(x.grad, z.grad, y.grad)
-'''
+
 print(d)
 print(d.parents)
 print(type(d.parents[0]))
@@ -81,4 +90,20 @@ print(a+b)
 print(type(a+b))
 print(v.data)
 print(a.data) #print always returns a str
-'''
+''''
+t = Value(0)
+out = t.tanh()
+out.backward()
+print(out.data, t.grad)
+
+eps = 1e-4
+x1 = Value(0.8 + eps)
+x2 = Value(0.8 - eps)
+numerical_grad = (x1.tanh().data - x2.tanh().data)/(2*eps)
+
+x = Value(0.8)
+out = x.tanh()
+out.backward()
+
+print("analytical:", x.grad)
+print("numerical:", numerical_grad)
